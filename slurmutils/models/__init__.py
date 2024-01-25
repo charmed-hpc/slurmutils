a1b48f255a5b9cb35107abd5e34656fafb7975ea
Jason C. Nucciarone <jason.nucciarone@canonical.com>
2024-01-25 13:53:18 -0500

    feat!: Add data models
    BREAKING CHANGE: No longer use the token lib that was used to classify configuration knobs in slurm.conf. Data models are no longer dynamically generated at runtime.
    
    Signed-off-by: Jason C. Nucciarone <jason.nucciarone@canonical.com>

---
+++ slurmutils/models/__init__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Data models for common Slurm objects."""
+
+from .slurm import DownNodes, FrontendNode, Node, NodeSet, Partition, SlurmConfig
+from .slurmdbd import SlurmdbdConfig
