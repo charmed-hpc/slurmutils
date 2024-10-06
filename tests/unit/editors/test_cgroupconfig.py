33866634d7c900a5e38d9a576a4ae498f8633bd8
Jason C. Nucciarone <nuccitheboss@ubuntu.com>
2024-10-06 10:59:59 -0400

    tests(editors): use `pyfakefs` instead of creating temp file
    Use `pyfakefs` to create a mock Slurm configuration file where we would
    expect it to be located on a node. Also helps for testing file permissions
    as we don't need to mess with privileges on the host filesystem.
    
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>

--- tests/unit/editors/test_cgroupconfig.py
+++ tests/unit/editors/test_cgroupconfig.py
@@ -15,30 +15,23 @@
 
 """Unit tests for the cgroup.conf editor."""
 
-import unittest
-from pathlib import Path
+
+from constants import EXAMPLE_CGROUP_CONFIG
+from pyfakefs.fake_filesystem_unittest import TestCase
 
 from slurmutils.editors import cgroupconfig
 
-EXAMPLE_CGROUP_CONF = """#
-# `cgroup.conf` file generated at 2024-09-18 15:10:44.652017 by slurmutils.
-#
-ConstrainCores=yes
-ConstrainDevices=yes
-ConstrainRAMSpace=yes
-ConstrainSwapSpace=yes
-"""
 
-
-class TestCgroupConfigEditor(unittest.TestCase):
+class TestCgroupConfigEditor(TestCase):
     """Unit tests for cgroup.conf file editor."""
 
     def setUp(self) -> None:
-        Path("cgroup.conf").write_text(EXAMPLE_CGROUP_CONF)
+        self.setUpPyfakefs()
+        self.fs.create_file("/etc/slurm/cgroup.conf", contents=EXAMPLE_CGROUP_CONFIG)
 
     def test_loads(self) -> None:
         """Test `loads` method of the cgroupconfig module."""
-        config = cgroupconfig.loads(EXAMPLE_CGROUP_CONF)
+        config = cgroupconfig.loads(EXAMPLE_CGROUP_CONFIG)
         self.assertEqual(config.constrain_cores, "yes")
         self.assertEqual(config.constrain_devices, "yes")
         self.assertEqual(config.constrain_ram_space, "yes")
@@ -46,24 +39,21 @@ class TestCgroupConfigEditor(unittest.TestCase):
 
     def test_dumps(self) -> None:
         """Test `dumps` method of the cgroupconfig module."""
-        config = cgroupconfig.loads(EXAMPLE_CGROUP_CONF)
+        config = cgroupconfig.loads(EXAMPLE_CGROUP_CONFIG)
         # The new config and old config should not be equal since the
         # timestamps in the header will be different.
-        self.assertNotEqual(cgroupconfig.dumps(config), EXAMPLE_CGROUP_CONF)
+        self.assertNotEqual(cgroupconfig.dumps(config), EXAMPLE_CGROUP_CONFIG)
 
     def test_edit(self) -> None:
         """Test `edit` context manager from the cgroupconfig module."""
-        with cgroupconfig.edit("cgroup.conf") as config:
+        with cgroupconfig.edit("/etc/slurm/cgroup.conf") as config:
             config.constrain_cores = "no"
             config.constrain_devices = "no"
             config.constrain_ram_space = "no"
             config.constrain_swap_space = "no"
 
-        config = cgroupconfig.load("cgroup.conf")
+        config = cgroupconfig.load("/etc/slurm/cgroup.conf")
         self.assertEqual(config.constrain_cores, "no")
         self.assertEqual(config.constrain_devices, "no")
         self.assertEqual(config.constrain_ram_space, "no")
         self.assertEqual(config.constrain_swap_space, "no")
-
-    def tearDown(self) -> None:
-        Path("cgroup.conf").unlink()
--- tests/unit/editors/test_slurmconfig.py
+++ tests/unit/editors/test_slurmconfig.py
@@ -15,72 +15,24 @@
 
 """Unit tests for the slurm.conf editor."""
 
-import unittest
-from pathlib import Path
+
+from constants import EXAMPLE_SLURM_CONFIG
+from pyfakefs.fake_filesystem_unittest import TestCase
 
 from slurmutils.editors import slurmconfig
 from slurmutils.models import DownNodes, Node, Partition
 
-example_slurm_conf = """#
-# `slurm.conf` file generated at 2024-01-30 17:18:36.171652 by slurmutils.
-#
-SlurmctldHost=juju-c9fc6f-0(10.152.28.20)
-SlurmctldHost=juju-c9fc6f-1(10.152.28.100)
 
-ClusterName=charmed-hpc
-AuthType=auth/munge
-Epilog=/usr/local/slurm/epilog
-Prolog=/usr/local/slurm/prolog
-FirstJobId=65536
-InactiveLimit=120
-JobCompType=jobcomp/filetxt
-JobCompLoc=/var/log/slurm/jobcomp
-KillWait=30
-MaxJobCount=10000
-MinJobAge=3600
-PluginDir=/usr/local/lib:/usr/local/slurm/lib
-ReturnToService=0
-SchedulerType=sched/backfill
-SlurmctldLogFile=/var/log/slurm/slurmctld.log
-SlurmdLogFile=/var/log/slurm/slurmd.log
-SlurmctldPort=7002
-SlurmdPort=7003
-SlurmdSpoolDir=/var/spool/slurmd.spool
-StateSaveLocation=/var/spool/slurm.state
-SwitchType=switch/none
-TmpFS=/tmp
-WaitTime=30
-
-#
-# Node configurations
-#
-NodeName=juju-c9fc6f-2 NodeAddr=10.152.28.48 CPUs=1 RealMemory=1000 TmpDisk=10000
-NodeName=juju-c9fc6f-3 NodeAddr=10.152.28.49 CPUs=1 RealMemory=1000 TmpDisk=10000
-NodeName=juju-c9fc6f-4 NodeAddr=10.152.28.50 CPUs=1 RealMemory=1000 TmpDisk=10000
-NodeName=juju-c9fc6f-5 NodeAddr=10.152.28.51 CPUs=1 RealMemory=1000 TmpDisk=10000
-
-#
-# Down node configurations
-#
-DownNodes=juju-c9fc6f-5 State=DOWN Reason="Maintenance Mode"
-
-#
-# Partition configurations
-#
-PartitionName=DEFAULT MaxTime=30 MaxNodes=10 State=UP
-PartitionName=batch Nodes=juju-c9fc6f-2,juju-c9fc6f-3,juju-c9fc6f-4,juju-c9fc6f-5 MinNodes=4 MaxTime=120 AllowGroups=admin
-"""
-
-
-class TestSlurmConfigEditor(unittest.TestCase):
+class TestSlurmConfigEditor(TestCase):
     """Unit tests for slurm.conf file editor."""
 
     def setUp(self) -> None:
-        Path("slurm.conf").write_text(example_slurm_conf)
+        self.setUpPyfakefs()
+        self.fs.create_file("/etc/slurm/slurm.conf", contents=EXAMPLE_SLURM_CONFIG)
 
     def test_loads(self) -> None:
         """Test `loads` method of the slurmconfig module."""
-        config = slurmconfig.loads(example_slurm_conf)
+        config = slurmconfig.loads(EXAMPLE_SLURM_CONFIG)
         self.assertListEqual(
             config.slurmctld_host, ["juju-c9fc6f-0(10.152.28.20)", "juju-c9fc6f-1(10.152.28.100)"]
         )
@@ -115,15 +67,15 @@ class TestSlurmConfigEditor(unittest.TestCase):
 
     def test_dumps(self) -> None:
         """Test `dumps` method of the slurmconfig module."""
-        config = slurmconfig.loads(example_slurm_conf)
+        config = slurmconfig.loads(EXAMPLE_SLURM_CONFIG)
         # The new config and old config should not be equal since the
         # timestamps in the header will be different.
-        self.assertNotEqual(slurmconfig.dumps(config), example_slurm_conf)
+        self.assertNotEqual(slurmconfig.dumps(config), EXAMPLE_SLURM_CONFIG)
 
     def test_edit(self) -> None:
         """Test `edit` context manager from the slurmconfig module."""
         # Test descriptors for `slurm.conf` configuration options.
-        with slurmconfig.edit("slurm.conf") as config:
+        with slurmconfig.edit("/etc/slurm/slurm.conf") as config:
             del config.inactive_limit
             config.max_job_count = 20000
             config.proctrack_type = "proctrack/linuxproc"
@@ -132,7 +84,7 @@ class TestSlurmConfigEditor(unittest.TestCase):
             del config.nodes["juju-c9fc6f-2"]
             config.nodes.update(new_node.dict())
 
-        config = slurmconfig.load("slurm.conf")
+        config = slurmconfig.load("/etc/slurm/slurm.conf")
         self.assertIsNone(config.inactive_limit)
         self.assertEqual(config.max_job_count, "20000")
         self.assertEqual(config.proctrack_type, "proctrack/linuxproc")
@@ -142,14 +94,14 @@ class TestSlurmConfigEditor(unittest.TestCase):
         )
         self.assertEqual(config.nodes["batch-0"]["NodeAddr"], "10.152.28.48")
 
-        with slurmconfig.edit("slurm.conf") as config:
+        with slurmconfig.edit("/etc/slurm/slurm.conf") as config:
             del config.nodes
             del config.frontend_nodes
             del config.down_nodes
             del config.node_sets
             del config.partitions
 
-        config = slurmconfig.load("slurm.conf")
+        config = slurmconfig.load("/etc/slurm/slurm.conf")
         self.assertDictEqual(config.nodes, {})
         self.assertDictEqual(config.frontend_nodes, {})
         self.assertListEqual(config.down_nodes, [])
@@ -226,7 +178,7 @@ class TestSlurmConfigEditor(unittest.TestCase):
             ),
         ]
 
-        with slurmconfig.edit("slurm.conf") as config:
+        with slurmconfig.edit("/etc/slurm/slurm.conf") as config:
             for node in new_nodes:
                 config.nodes.update(node.dict())
 
@@ -287,7 +239,7 @@ class TestSlurmConfigEditor(unittest.TestCase):
             },
         }
 
-        config = slurmconfig.loads(example_slurm_conf)
+        config = slurmconfig.loads(EXAMPLE_SLURM_CONFIG)
         updates = slurmconfig.SlurmConfig.from_dict(config_updates)
         config.update(updates)
 
@@ -378,6 +330,3 @@ class TestSlurmConfigEditor(unittest.TestCase):
                 },
             },
         )
-
-    def tearDown(self):
-        Path("slurm.conf").unlink()
