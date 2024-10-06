3ea275529d80ed52e2491caca4c5ce24d765b4c7
Jason C. Nucciarone <nuccitheboss@ubuntu.com>
2024-10-06 10:59:59 -0400

    tests(editors): use `pyfakefs` instead of creating temp file
    Use `pyfakefs` to create a mock Slurm configuration file where we would
    expect it to be located on a node. Also helps for testing file permissions
    as we don't need to mess with privileges on the host filesystem.
    
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>

--- tests/unit/editors/test_slurmdbdconfig.py
+++ tests/unit/editors/test_slurmdbdconfig.py
@@ -15,57 +15,23 @@
 
 """Unit tests for the slurmdbd.conf editor."""
 
-import unittest
-from pathlib import Path
+
+from constants import EXAMPLE_SLURMDBD_CONFIG
+from pyfakefs.fake_filesystem_unittest import TestCase
 
 from slurmutils.editors import slurmdbdconfig
 
-example_slurmdbd_conf = """#
-# `slurmdbd.conf` file generated at 2024-01-30 17:18:36.171652 by slurmutils.
-#
-ArchiveEvents=yes
-ArchiveJobs=yes
-ArchiveResvs=yes
-ArchiveSteps=no
-ArchiveTXN=no
-ArchiveUsage=no
-ArchiveScript=/usr/sbin/slurm.dbd.archive
-AuthInfo=/var/run/munge/munge.socket.2
-AuthType=auth/munge
-AuthAltTypes=auth/jwt
-AuthAltParameters=jwt_key=16549684561684@
-DbdHost=slurmdbd-0
-DbdBackupHost=slurmdbd-1
-DebugLevel=info
-PluginDir=/all/these/cool/plugins
-PurgeEventAfter=1month
-PurgeJobAfter=12month
-PurgeResvAfter=1month
-PurgeStepAfter=1month
-PurgeSuspendAfter=1month
-PurgeTXNAfter=12month
-PurgeUsageAfter=24month
-LogFile=/var/log/slurmdbd.log
-PidFile=/var/run/slurmdbd.pid
-SlurmUser=slurm
-StoragePass=supersecretpasswd
-StorageType=accounting_storage/mysql
-StorageUser=slurm
-StorageHost=127.0.0.1
-StoragePort=3306
-StorageLoc=slurm_acct_db
-"""
 
-
-class TestSlurmdbdConfigEditor(unittest.TestCase):
+class TestSlurmdbdConfigEditor(TestCase):
     """Unit tests for the slurmdbd.conf file editor."""
 
     def setUp(self) -> None:
-        Path("slurmdbd.conf").write_text(example_slurmdbd_conf)
+        self.setUpPyfakefs()
+        self.fs.create_file("/etc/slurm/slurmdbd.conf", contents=EXAMPLE_SLURMDBD_CONFIG)
 
     def test_loads(self) -> None:
         """Test `loads` method of the slurmdbdconfig module."""
-        config = slurmdbdconfig.loads(example_slurmdbd_conf)
+        config = slurmdbdconfig.loads(EXAMPLE_SLURMDBD_CONFIG)
         self.assertListEqual(config.plugin_dir, ["/all/these/cool/plugins"])
         self.assertDictEqual(config.auth_alt_parameters, {"jwt_key": "16549684561684@"})
         self.assertEqual(config.slurm_user, "slurm")
@@ -73,26 +39,23 @@ class TestSlurmdbdConfigEditor(unittest.TestCase):
 
     def test_dumps(self) -> None:
         """Test `dumps` method of the slurmdbdconfig module."""
-        config = slurmdbdconfig.loads(example_slurmdbd_conf)
+        config = slurmdbdconfig.loads(EXAMPLE_SLURMDBD_CONFIG)
         # The new config and old config should not be equal since the
         # timestamps in the header will be different.
-        self.assertNotEqual(slurmdbdconfig.dumps(config), example_slurmdbd_conf)
+        self.assertNotEqual(slurmdbdconfig.dumps(config), EXAMPLE_SLURMDBD_CONFIG)
 
     def test_edit(self) -> None:
         """Test `edit` context manager from the slurmdbdconfig module."""
-        with slurmdbdconfig.edit("slurmdbd.conf") as config:
+        with slurmdbdconfig.edit("/etc/slurm/slurmdbd.conf") as config:
             config.archive_usage = "yes"
             config.log_file = "/var/spool/slurmdbd.log"
             config.debug_flags = ["DB_EVENT", "DB_JOB", "DB_USAGE"]
             del config.auth_alt_types
             del config.auth_alt_parameters
 
-        config = slurmdbdconfig.load("slurmdbd.conf")
+        config = slurmdbdconfig.load("/etc/slurm/slurmdbd.conf")
         self.assertEqual(config.archive_usage, "yes")
         self.assertEqual(config.log_file, "/var/spool/slurmdbd.log")
         self.assertListEqual(config.debug_flags, ["DB_EVENT", "DB_JOB", "DB_USAGE"])
         self.assertIsNone(config.auth_alt_types)
         self.assertIsNone(config.auth_alt_parameters)
-
-    def tearDown(self) -> None:
-        Path("slurmdbd.conf").unlink()
